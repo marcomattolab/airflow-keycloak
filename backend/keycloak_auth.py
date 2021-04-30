39995946fb8bfdc9989e8ceebabe0b7f4d1fb32f
Marco Martorana <marcomatto@libero.it>
2021-04-30 16:11:44 +0200

    Create keycloak_auth.py

---
+++ backend/keycloak_auth.py
@@ -0,0 +1,85 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Keycloak authentication backend"""
+from functools import wraps
+from typing import Callable, Optional, Tuple, TypeVar, Union, cast
+from flask import Response, current_app, request
+from flask_appbuilder.security.sqla.models import User
+from flask_login import login_user
+from requests.auth import AuthBase
+
+import requests
+import json
+
+CLIENT_AUTH: Optional[Union[Tuple[str, str], AuthBase]] = None
+
+
+def init_app(_):
+    """Initializes authentication backend"""
+
+
+T = TypeVar("T", bound=Callable)  # pylint: disable=invalid-name
+
+
+def auth_current_user() -> Optional[User]:
+    #print("\n### 1. keycloak_auth.auth_current_user() ")
+    """Authenticate and set current user if Authorization header exists"""
+    auth = request.authorization
+    #print(f"\n### 2. keycloak_auth.auth_current_user() => request.authorization: {auth} ")
+	
+    if auth is None or not auth.username or not auth.password:
+        return None
+
+    ab_security_manager = current_app.appbuilder.sm
+    user = None
+    if user is None:
+        oauth_providers = ab_security_manager.getOauthParams()
+        
+        url = oauth_providers["access_token_url"]
+        reqToken = {'client_id': oauth_providers["client_id"], 
+					'client_secret': oauth_providers["client_secret"],
+					'username': auth.username,
+					'password': auth.password,
+					'grant_type': 'password'}
+        resToken = requests.post(url, data = reqToken)
+        resTokenJSON = json.loads(resToken.text)
+        access_token = resTokenJSON['access_token']
+        
+        url = oauth_providers["api_base_url"] + "userinfo"
+        reqUserinfo = {'access_token': access_token}
+        resUserinfo = requests.post(url, data = reqUserinfo)
+        resUserinfoJSON = json.loads(resUserinfo.text)
+        resUserinfoJSON['username'] = auth.username
+        
+        user = ab_security_manager.auth_user_oauth(resUserinfoJSON)
+        #print(f"\n### 3. keycloak_auth.auth_user_oauth() => {user} \n")
+    if user is not None:
+        login_user(user, remember=False)
+    return user
+
+
+def requires_authentication(function: T):
+    """Decorator for functions that require authentication"""
+
+    @wraps(function)
+    def decorated(*args, **kwargs):
+        if auth_current_user() is not None:
+            return function(*args, **kwargs)
+        else:
+            return Response("Unauthorized", 401, {"WWW-Authenticate": "Basic"})
+
+    return cast(T, decorated)
