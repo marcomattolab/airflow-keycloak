f4685205d647b3cbd97ee331492fa60f715bd7c6
Marco Martorana <marcomatto@libero.it>
2021-05-05 19:20:36 +0200

    Update keycloak_auth.py
    Add Keycloak authentication

--- backend/keycloak_auth.py
+++ backend/keycloak_auth.py
@@ -1,19 +1,12 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
+# Company: Engineering Ingegneria Informatica S.p.A.
+# Class: keycloak_auth.py 
+# Description: 
+#    This is the authentication backend working as authenticator with Keycloak (IDM).
+#    The authentication backend uses a valid "Bearer" token (if provided in headers/authorization) 
+#    otherwise uses credentials: username and password  (if provided in request/authorization)
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+# License: http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
 """Keycloak authentication backend"""
 from functools import wraps
 from typing import Callable, Optional, Tuple, TypeVar, Union, cast
@@ -39,18 +32,19 @@ def auth_current_user() -> Optional[User]:
     print("\n## 1. keycloak_auth.auth_current_user() ")
     """Authenticate and set current user if Authorization header exists"""
     auth = request.authorization
-    print(f"\n## 2. keycloak_auth.auth_current_user() => request.authorization: {auth} ")
-	
-    #if auth is None or not auth.username or not auth.password:
-    if auth is None or ((not 'access_token' in auth or not 'token' in auth) and (not 'username' in auth and not 'password' in auth)):
-        print(f"\n## 2.5 FAILED AUTH - NO MANDATORY PARAMETERS: (username + password | token | access_token ) \n\n")
+    print(f"\n## 2. keycloak_auth.auth_current_user() => request.authorization: {request.authorization}  -  request.headers.get('Authorization'): {request.headers.get('Authorization')} ")
+    bearer_array = request.headers.get('Authorization').split(" ") if request.headers.get('Authorization') is not None else []
+    bearer = bearer_array[1] if len(bearer_array)==2 and bearer_array[0]=="Bearer" else None
+    
+    if bearer is None and not 'username' in auth and not 'password' in auth:
+        print(f"\n## 2.5 FAILED AUTH - NO MANDATORY PARAMETERS: (username and password | Bearer ) ")
         return None
 
     ab_security_manager = current_app.appbuilder.sm
     user = None
     if user is None:
         oauth_providers = ab_security_manager.getOauthParams()
-        if not 'access_token' in auth and not 'token' in auth:
+        if bearer is None:
             url = oauth_providers["access_token_url"]
             reqToken = {'client_id': oauth_providers["client_id"], 
             			'client_secret': oauth_providers["client_secret"],
@@ -60,20 +54,20 @@ def auth_current_user() -> Optional[User]:
             resToken = requests.post(url, data = reqToken)
             resTokenJSON = json.loads(resToken.text)
             access_token = resTokenJSON['access_token']
-            print(f"\n## 3.A  access_token NOT in auth (called oauth) => {access_token}")
+            print(f"\n## 3.A  access_token retrieved from keycloak (IDM) by credentials => {access_token}")
         else:
-            print(f"\n## 3.B  access_token/token in auth => {auth}")
-            access_token = auth.access_token if 'access_token' in auth else auth.token
+            print(f"\n## 3.B  access_token equals to Bearer retrieved from headers => {bearer}")
+            access_token = bearer
 
         url = oauth_providers["api_base_url"] + "userinfo"
         reqUserinfo = {'access_token': access_token}
         resUserinfo = requests.post(url, data = reqUserinfo)
         resUserinfoJSON = json.loads(resUserinfo.text)
-        print(f"\n\n## 4. USERINFO is => {resUserinfoJSON}")
+        print(f"\n\n## 4. USERINFO (Json) retrieved => {resUserinfoJSON}")
         resUserinfoJSON['username'] = resUserinfoJSON['preferred_username']
         
         user = ab_security_manager.auth_user_oauth(resUserinfoJSON)
-        print(f"\n## 5. keycloak_auth.auth_user_oauth() user is => {user} \n")
+        print(f"\n## 5. keycloak_auth.auth_user_oauth() authenticated user is => {user} ")
     if user is not None:
         login_user(user, remember=False)
     return user
